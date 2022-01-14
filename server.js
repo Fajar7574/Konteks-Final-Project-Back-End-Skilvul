5a25c96dbb962422f0596f0f55850b2cf850ab67
Fajar7574 <16017337.fajar.ridwan@gmail>
2022-01-15 01:00:04 +0700

    deploy

--- server.js
+++ server.js
@@ -39,8 +39,6 @@ require("./app/routes/newscampaign.routes")(app);
 require("./app/routes/rule.routes")(app);
 require("./app/routes/campaignusermessage.routes")(app);
 
-// set port, listen for requests
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
